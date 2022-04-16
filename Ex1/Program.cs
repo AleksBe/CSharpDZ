47689ccc52b4ba7c36e1a9f22fe96b84f932118d
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-16 14:12:19 +0300

    Variant1

---
+++ Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 4;
+int b = 4;
+
+int max = a;
+int min = b;
+
+if (a > b) max = a; min = b;
+if (b > a) max = b; min = a;
+
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
+Console.Write("Минимальное число = ");
+Console.WriteLine(min);
+
+if (a == b) Console.WriteLine("Данные числа равны между собой"); // Импровизация, чтобы выдавался результат и в этом случае.
