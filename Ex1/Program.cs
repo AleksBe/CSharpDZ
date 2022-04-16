5d275778a352c75315dadbe4df7313cdf0ce20b1
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-16 15:15:05 +0300

    Variant2

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,16 +1,18 @@
-﻿int a = 4;
-int b = 4;
+﻿int a = 5;
+int b = 5;
 
-int max = a;
-int min = b;
-
-if (a > b) max = a; min = b;
-if (b > a) max = b; min = a;
-
-
-Console.Write("Максимальное число = ");
-Console.WriteLine(max);
-Console.Write("Минимальное число = ");
-Console.WriteLine(min);
-
+if (a > b)
+{          
+           Console.Write("Максимальное число = ");
+           Console.WriteLine(a);
+           Console.Write("Минимальное число = ");
+           Console.WriteLine(b);
+}
+if (b > a) 
+{          
+           Console.Write("Максимальное число = ");
+           Console.WriteLine(b);
+           Console.Write("Минимальное число = ");
+           Console.WriteLine(a);
+}
 if (a == b) Console.WriteLine("Данные числа равны между собой"); // Импровизация, чтобы выдавался результат и в этом случае.
