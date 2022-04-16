ea32315c5278fbd8896ab3bc1fa99ac99f12e3d3
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-16 16:48:59 +0300

    Variant

---
+++ Ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿int N = 10;
+int a = 1;
+int count = 0;
+
+while(a <= N) // будем считать, что до N включительно
+{
+    if(a % 2 == 0)
+    {
+        count = count +1;
+    }
+    a = a + 1;
+} 
+
+Console.Write("В указанном интервале количество четных чисел= ");
+Console.WriteLine(count);
